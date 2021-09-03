3325de5585190f5a7317d925a9ab8360a1bead6c
Lauri Juvela <lauri.juvela@gmail.com>
2021-09-03 20:49:09 +0300

    update tests

--- python/convolution_layer.py
+++ python/convolution_layer.py
@@ -5,23 +5,24 @@ from glotnet.convolution import Convolution
 class ConvolutionLayerFunction(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input, weight_conv, bias_conv, weight_out, bias_out, dilation):
+    def forward(ctx, input, weight_conv, bias_conv, weight_out, bias_out, dilation, activation, use_output_transform):
 
         input = input.contiguous()
-        # weights (OUT_CH, IN_CH, W) -> (W, CH_IN, CH_OUT)
+        # weights (OUT_CH, IN_CH, W) -> (W, IN_CH, OUT_CH)
         weight_conv = weight_conv.permute(2, 1, 0).contiguous()
         weight_out = weight_out.permute(2, 1, 0).contiguous()
         bias_conv = bias_conv.contiguous()
         bias_out = bias_out.contiguous()
-        ctx.save_for_backward(input, weight_conv, bias_conv, weight_out, bias_out, dilation)
+        ctx.save_for_backward(input, weight_conv, bias_conv,
+                              weight_out, bias_out,
+                              torch.tensor(dilation))
         
+        print(f"Conv weight shape {weight_conv.shape}")
+
         training = False
-        use_residual = True
-        activation = "gated"
         output, skip = ext.convolution_layer_forward(
             input, weight_conv, bias_conv, weight_out, bias_out,
-            training, dilation, use_residual, activation)
-        import ipdb; ipdb.set_trace()
+            training, dilation, use_output_transform, activation)
         return output, skip
 
     def backward(self, d_output, d_skip):
@@ -36,15 +37,40 @@ class ConvolutionLayer(torch.nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size, dilation=1, bias=True, device=None, dtype=None,
                  causal=True,
-                 training=True,):
+                 training=True,
+                 activation="gated",
+                 use_output_transform=True,
+                 ):
         super().__init__()
 
         self.training = training
         residual_channels = out_channels
-        self.conv = Convolution(in_channels=in_channels, out_channels=2*residual_channels, kernel_size=kernel_size, dilation=dilation, bias=bias, device=device, dtype=dtype, causal=causal, training=training)
-        self.out = Convolution(in_channels=residual_channels, out_channels=residual_channels, kernel_size=1, dilation=1, bias=bias, device=device, dtype=dtype, training=training)
+        self.activation = activation
+        self.activation_fun, self.channel_mul = self._parse_activation(activation)
+        self.conv = Convolution(
+            in_channels=in_channels,
+            out_channels=self.channel_mul*residual_channels,
+            kernel_size=kernel_size, dilation=dilation, bias=bias, device=device, dtype=dtype,
+            causal=causal, training=training)
+        self.out = Convolution(
+            in_channels=residual_channels,
+            out_channels=residual_channels,
+            kernel_size=1, dilation=1, bias=bias, device=device, dtype=dtype,
+            training=training)
         self.residual_channels = residual_channels
         self.dilation = dilation
+        self.use_output_transform = use_output_transform
+        
+    def _parse_activation(self, activation):
+        activations = {
+            "gated" : ((torch.tanh, torch.sigmoid), 2),
+            "tanh" : (torch.tanh, 1),
+            "linear" : (torch.nn.Identity(), 1)
+        }
+        activation_fun, channel_mul = activations.get(activation, (None, None))
+        if channel_mul is None:
+            raise NotImplementedError
+        return activation_fun, channel_mul
 
     def forward(self, input, training=None):
         """ 
@@ -64,15 +90,22 @@ class ConvolutionLayer(torch.nn.Module):
             self.training = training
 
         if self.training:
-            R = self.residual_channels
+            
             x = self.conv(input)
-            x = torch.tanh(x[:, :R, :]) * torch.sigmoid(x[:, R:, :])
+            if self.channel_mul == 2:
+                R = self.residual_channels
+                x = self.activation_fun[0](x[:, :R, :]) * self.activation_fun[1](x[:, R:, :])
+            else:
+                x = self.activation_fun(x)
             skip = x
-            output = self.out(x) + input
+            if self.use_output_transform:
+                output = self.out(x)
+            else:
+                output = x
             return output, skip
         else:
             output, skip = ConvolutionLayerFunction.apply(input, self.conv.weight, self.conv.bias,
-            self.out.weight, self.out.bias, self.dilation)
+            self.out.weight, self.out.bias, self.dilation, self.activation, self.use_output_transform)
             return output, skip
 
 
--- test/test_convolution_layer.py
+++ test/test_convolution_layer.py
@@ -2,9 +2,27 @@ import torch
 
 from glotnet.convolution_layer import ConvolutionLayer
 
+if __name__ == "__main__":
+    print("Test layer with linear activations")
+    torch.manual_seed(42)
+    timesteps = 100
+    batch = 1 
+    in_channels = 1
+    out_channels= 1
+    kernel_size = 20
+    dilation = 1
+    x = 0.1 * torch.randn(batch, in_channels, timesteps)
+    conv = ConvolutionLayer(in_channels, out_channels, kernel_size,
+                            dilation=dilation, activation="linear")
+    y1, s1 = conv(x, training=True)
+    y2, s2 = conv(x, training=False)
+    assert torch.allclose(s1, s2, atol=1e-6, rtol=1e-5), "Assert skip output match"
+    assert torch.allclose(y1, y2, atol=1e-6, rtol=1e-5), "Assert main outputs match"
+    print("   ok!")
 
 # def test_forward1():
 if __name__ == "__main__":
+    print("Test layer with linear activations")
     torch.manual_seed(42)
     timesteps = 100
     batch = 1 
@@ -12,13 +30,32 @@ if __name__ == "__main__":
     out_channels= 1
     kernel_size = 50
     dilation = 1
-    x = torch.randn(batch, in_channels, timesteps)
-    conv = ConvolutionLayer(in_channels, out_channels, kernel_size, dilation=dilation)
+    x = 0.1 * torch.randn(batch, in_channels, timesteps)
+    conv = ConvolutionLayer(in_channels, out_channels,
+                            kernel_size, dilation=dilation, activation="tanh")
     y1, s1 = conv(x, training=True)
     y2, s2 = conv(x, training=False)
-    # assert torch.allclose(y1, y2)
+    assert torch.allclose(s1, s2, atol=1e-6, rtol=1e-5), "Assert skip output match"
+    assert torch.allclose(y1, y2, atol=1e-6, rtol=1e-5), "Assert main outputs match"
+    print("   ok!")
 
-
+if __name__ == "__main__":
+    print("Test layer with gated activations")
+    torch.manual_seed(42)
+    timesteps = 100
+    batch = 1 
+    in_channels = 1
+    out_channels= 1
+    kernel_size = 50
+    dilation = 1
+    x = 0.1 * torch.randn(batch, in_channels, timesteps)
+    conv = ConvolutionLayer(in_channels, out_channels,
+                            kernel_size, dilation=dilation, activation="gated")
+    y1, s1 = conv(x, training=True)
+    y2, s2 = conv(x, training=False)
+    assert torch.allclose(s1, s2, atol=1e-6, rtol=1e-5), "Assert skip output match"
+    assert torch.allclose(y1, y2, atol=1e-6, rtol=1e-5), "Assert main outputs match"
+    print("   ok!")
 
 # if __name__ == "__main__":
     # print("Testing forward pass with SISO, batch size 1")
