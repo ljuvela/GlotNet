03710952cb837557797dac00226e298e21948526
Lauri Juvela <lauri.juvela@gmail.com>
2022-05-17 20:47:24 +0300

    add test for Gaussian NLL

---
+++ glotnet/losses/distributions.py
@@ -0,0 +1,39 @@
+import torch
+from glotnet.model.convolution import Convolution
+
+class GaussianDensity(torch.nn.Module):
+
+    def __init__(self, num_bits=None):
+        """
+        Args:
+            input_channels: number of input channels
+            num_bits: number of bits used to calculate entropy floor
+        """
+        super().__init__()
+        self.num_bits = num_bits
+
+        self.register_buffer('const', torch.tensor(2 * torch.pi).sqrt().log())
+
+    def nll(self, x, params):
+        """
+        
+        """
+        # calculate entropy floor hinge regularizer
+
+
+        #
+
+        # NLL
+        m = params[..., 0:1]
+        log_s = params[..., 1:2]
+        s = torch.exp(log_s)
+
+        nll = 0.5 * (m - x).div(s).pow(2) + log_s + self.const
+        return nll
+
+    def forward(self, x, params):
+        return self.nll(x, params)
+
+    def sample(self, params):
+        pass
+
---
+++ test/test_distribution/test_distribution.py
@@ -0,0 +1,33 @@
+import torch
+
+from torch.distributions.normal import Normal as NormalRef
+from glotnet.losses.distributions import GaussianDensity
+
+def power_spectral_density():
+    """ PSD for testing sampling """
+    pass
+
+def test_gaussian():
+
+    batch = 2
+    num_samples = 10
+    x = torch.randn(batch, num_samples, 1)
+    mu = torch.randn(batch, num_samples, 1)
+    log_sigma = torch.clamp(torch.randn(batch, num_samples, 1), min=-7.0)
+    sigma = torch.exp(log_sigma)
+    
+    dist_ref = NormalRef(loc=mu, scale=sigma)
+    dist = GaussianDensity()
+
+    # negative log likelihood from reference
+    nll_ref = -1.0 * dist_ref.log_prob(x)
+    # negative log likelihood from custom implementation
+    params = torch.cat([mu, log_sigma], dim=-1)
+    nll = dist.nll(x, params)
+
+    assert torch.allclose(nll_ref, nll), \
+        f"NLL must match \n ref: {nll_ref} \n custom: {nll}"
+
+if __name__ == "__main__":
+
+    test_gaussian()
