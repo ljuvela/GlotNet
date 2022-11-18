d10e04680c358d8a94e4325a5e4f13276bc828b0
Lauri Juvela <lauri.juvela@gmail.com>
2022-11-18 22:43:36 +0200

    allpole explicit backward tested and verified

--- glotnet/sigproc/allpole.py
+++ glotnet/sigproc/allpole.py
@@ -1,5 +1,6 @@
 import torch
-import torchaudio.functional as F
+import torchaudio.functional as Fa
+import torch.nn.functional as F
 
 def allpole(x: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
     """
@@ -30,13 +31,80 @@ def allpole(x: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
 
     return y[..., p:]
 
+class AllPoleFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, a):
+        y = allpole(x, a)
+        ctx.save_for_backward(y, x, a)
+        return y
+
+    @staticmethod
+    def backward(ctx, dy):
+        y, x, a = ctx.saved_tensors
+        dx = da = None
+
+        n_batch = x.size(0)
+        n_channels = x.size(1)
+        n_order = a.size(-1) - 1
+
+        # filter or
+        dyda = allpole(-y, a)
+        dyda = torch.nn.functional.pad(dyda, (n_order, 0))
+
+        if a.requires_grad:
+            da = F.conv1d(
+                dyda.view(1, n_batch * n_channels, -1),
+                dy.view(n_batch * n_channels, 1, -1),
+                groups=n_batch * n_channels).view(n_batch, n_channels, -1).sum(0).flip(1)
+            
+        if x.requires_grad:
+            dx = allpole(dy.flip(-1), a).flip(-1)
+
+        return dx, da
+
+class AllPole(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, a):
+        return AllPoleFunction.apply(x, a)
+
+def test_allpole_class():
+
+    x = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) * 1.0
+    a = torch.tensor([1, -0.5, 0.2])
+
+    a_ref = a.clone().detach().requires_grad_(True)
+    x_ref = x.clone().detach().requires_grad_(True)
+
+    x.requires_grad_(True)
+    a.requires_grad_(True)
+
+    allpole = AllPole()
+
+    y = allpole(x.unsqueeze(0).unsqueeze(0),  a.unsqueeze(0))
+    y.sum().backward()
+
+    # test autograd against torchaudio.functional.lfilter
+    b = torch.zeros_like(a_ref)
+    b[0] = 1
+    y_ref = Fa.lfilter(x_ref.unsqueeze(0), a_ref.unsqueeze(0), b.unsqueeze(0), clamp=False).squeeze(0)
+    y_ref.sum().backward()
+
+    assert torch.allclose(y, y_ref)
+
+    assert x.grad is not None
+    assert a.grad is not None
+
+    # check that gradients are the same
+    assert torch.allclose(x.grad, x_ref.grad)
+    assert torch.allclose(a.grad, a_ref.grad)
 
 
 def test_allpole():
-    # test against scipy.signal.lfilter
 
-
-    import scipy.signal
     x = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) * 1.0
     a = torch.tensor([1, -0.5, 0.2])
     # a = torch.tensor([1, -0.5])
@@ -45,9 +113,10 @@ def test_allpole():
     # test against torchaudio.functional.lfilter
     b = torch.zeros_like(a)
     b[0] = 1
-    y2 = F.lfilter(x.unsqueeze(0), a.unsqueeze(0), b.unsqueeze(0), clamp=False).squeeze(0)
+    y2 = Fa.lfilter(x.unsqueeze(0), a.unsqueeze(0), b.unsqueeze(0), clamp=False).squeeze(0)
 
     assert torch.allclose(y, y2)
 
 if __name__ == "__main__":
     test_allpole()
+    test_allpole_class()
