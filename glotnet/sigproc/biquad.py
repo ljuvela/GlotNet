126d077ff269be3a8d07a70b7c003e69393f2ee0
Lauri Juvela <lauri.juvela@aalto.fi>
2023-01-25 16:34:29 +0200

    biquad bank wip

--- glotnet/sigproc/biquad.py
+++ glotnet/sigproc/biquad.py
@@ -1,12 +1,13 @@
 import torch
 
 from .lfilter import LFilter
-from typing import Tuple
+from typing import Tuple, Union
 
 
 class BiquadBandPassFunctional(torch.nn.Module):
 
     def __init__(self):
+        """ Initialize Biquad"""
 
         super().__init__()
 
@@ -44,8 +45,12 @@ class BiquadBandPassFunctional(torch.nn.Module):
         a1 = -2.0 * torch.cos(omega)
         a2 = 1 - alpha / A
 
+
+        # TODO: unsqueeze for multichannel
         a = torch.cat([a0, a1, a2], dim=-1)
         b = torch.cat([b0, b1, b2], dim=-1)
+        print(f"{a.shape}")
+        print(f"{b.shape}")
         b = b / a0
         a = a / a0
         return b, a
@@ -69,15 +74,13 @@ class BiquadBandPassFunctional(torch.nn.Module):
                 shape = (batch, channels, n_filters, n_frames)
         """
 
-
-
         b, a = self._params_to_direct_form(freq=freq, gain=gain, Q=Q)
         num_frames = x.size(-1) // self.hop_length
         if b.ndim < 3:
             b = b.reshape(1, -1, 1).expand(-1, -1, num_frames)
         if a.ndim < 3:
             a = a.reshape(1, -1, 1).expand(-1, -1, num_frames)
-        
+
         # reshape parallel filters to batch
 
         # expand input for parallel filters
@@ -89,19 +92,48 @@ class BiquadBandPassFunctional(torch.nn.Module):
 
 class BiquadBandPassModule(BiquadBandPassFunctional):
 
-    def __init__(self, freq: float = 0.5, gain: float = 0.0, Q: float = 0.7071):
+    def __init__(self,
+                 freq: Union[torch.Tensor, float] = torch.tensor([0.5]),
+                 gain: Union[torch.Tensor, float] = torch.tensor([0.0]),
+                 Q: Union[torch.Tensor, float] = torch.tensor([0.7071]),
+                 fs: float = None):
         """
         Args:
-            freq: normalized frequency from 0 to 1 (Nyquist)
+            freq: center frequency 
             gain: gain in dB
             Q: quality factor determining filter resonance bandwidth
+            fs: sample rate, if not provided freq is assumed as normalized from 0 to 1 (Nyquist)
+
         """
         super().__init__()
 
-        # TODO: separate wrapper module for self-contained parameters (inherit biquad functional?)
-        self.gain_dB = torch.nn.Parameter(torch.tensor(gain, dtype=torch.float32).reshape(1, 1))
-        self.freq = torch.nn.Parameter(torch.tensor(freq, dtype=torch.float32).reshape(1, 1))
-        self.Q = torch.nn.Parameter(torch.tensor(Q, dtype=torch.float32).reshape(1, 1))
+        # if no sample rate provided, assume normalized frequency
+        if fs is None:
+            fs = 2.0
+
+        self.fs = fs
+
+        if type(freq) == float:
+            freq = torch.tensor([freq])
+        if type(gain) == float:
+            gain = torch.tensor([gain])
+        if type(Q) == float:
+            Q = torch.tensor([Q])
+
+        # convert to normalized frequency
+        freq = 2.0 * freq / fs
+
+        if freq.max() > 1.0:
+            raise ValueError(
+                "Maximum normalized frequency is larger than 1.0. "
+                "Please provide a sample rate or input normalized frequencies")
+        if freq.min() < 0.0:
+            raise ValueError(
+                "Maximum normalized frequency is smaller than 0.0.")
+
+        self.gain_dB = torch.nn.Parameter(gain.unsqueeze(0))
+        self.freq = torch.nn.Parameter(freq.unsqueeze(0))
+        self.Q = torch.nn.Parameter(Q.unsqueeze(0))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
@@ -109,9 +141,14 @@ class BiquadBandPassModule(BiquadBandPassFunctional):
         gain = self.gain_dB
         Q = self.Q
 
-        # TODO reshape inputs
+        timesteps = x.size(2)
+        num_frames = timesteps // self.hop_length
 
-        y = super.forward(x, freq, gain, Q)
+        # reshape parameters
+        # freq.expand()
+
+        import ipdb; ipdb.set_trace()
+        y = super().forward(x, freq, gain, Q)
 
         # TODO reshape outputs
 
--- test/test_sigproc/test_biquad.py
+++ test/test_sigproc/test_biquad.py
@@ -5,17 +5,19 @@ import glotnet.sigproc.biquad; reload(glotnet.sigproc.biquad)
 from glotnet.sigproc.biquad import BiquadBandPassFunctional, BiquadBandPassModule
 from glotnet.sigproc.oscillator import Oscillator
 
+import pytest
 
 def test_peak_biquad_gains():
 
 
     fs = 16000
     f0 = 100
+    timesteps = 2048
 
     osc = Oscillator(audio_rate=fs, control_rate=fs)
 
-    x_100 = osc(f0=100 * torch.ones(1, 1, 2048))
-    x_2000 = osc(f0=2000 * torch.ones(1, 1, 2048))
+    x_100 = osc(f0=100 * torch.ones(1, 1, timesteps))
+    x_2000 = osc(f0=2000 * torch.ones(1, 1, timesteps))
 
     # gain in dB
     gain = 6.0
@@ -23,8 +25,44 @@ def test_peak_biquad_gains():
     freq = f0 / (fs / 2)
     biquad = BiquadBandPassModule(freq=freq, gain=gain)
     
+    # pass throught biquad
     y_100 = biquad(x_100)
     y_2000 = biquad(x_2000)
 
     assert (y_100.max() - 10 ** (0.05 * gain)).abs() < 0.01
     assert (y_2000.max() - 1).abs() < 0.05
+
+
+# TODO: test invalid normalized frequency (skip fs)
+
+def test_peak_biquad_bank():
+    fs = 16000
+    batch = 1
+    channels = 1
+    timesteps = 2048
+
+    # bank of biquads
+    freq = torch.tensor([100.0, 1000.0, 2000.0])
+    gain = torch.tensor([6.0, -6.0, 12.0])
+
+    biquad = BiquadBandPassModule(freq=freq, gain=gain, fs=fs)
+
+    x = torch.randn(batch, channels, timesteps)
+
+    y = biquad.forward(x)
+    
+
+def test_biquad_backprop():
+
+    pass
+
+
+def test_peak_biquad_variable_freq():
+
+    biquad = BiquadBandPassFunctional()
+
+
+
+
+
+    
