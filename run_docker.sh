0372a115604cecf037d1aeeb04e79e385f239f1d
Pablo Perez <pablopz@kth.se>
2023-02-08 10:34:51 +0100

    Include git submodule update in container initialization

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --gpus '"device=0,1,2,3"' --ipc=host -v $PWD:/workspace/dockers -v $PWD/../Data/LJSpeech:/workspace/dockers/Dataset --name pablopz_glot1 pablopz_glotnet /bin/sh -c 'cd dockers; pip install -v -e .; pytest test; bash'
+docker run -it --rm --gpus '"device=0,1,2,3"' --ipc=host -v $PWD:/workspace/dockers -v $PWD/../Data/LJSpeech:/workspace/dockers/Dataset --name pablopz_glot1 pablopz_glotnet /bin/sh -c 'cd dockers; git submodule update --recursive --init; pip install -v -e .; pytest test; bash'
