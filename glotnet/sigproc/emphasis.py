57541df55f014f74fab4320367d339cb76623025
Lauri Juvela <lauri.juvela@gmail.com>
2023-02-08 21:38:17 +0200

    add pre-emphasis module

---
+++ glotnet/sigproc/emphasis.py
@@ -0,0 +1,52 @@
+from .lfilter import LFilter
+import torch
+from .lfilter import ceil_division
+
+class Emphasis(torch.nn.Module):
+    """ Pre-emphasis and de-emphasis filter"""
+
+    def __init__(self, alpha=0.85) -> None:
+        """
+        Args:
+            alpha : pre-emphasis coefficient
+        """
+        super().__init__()
+        self.lfilter = LFilter(n_fft=512, hop_length=256, win_length=512)
+        self.register_buffer('coeffs', torch.tensor([1, -alpha], dtype=torch.float32))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """" Apply pre-emphasis to signal
+        Args:
+            x : input signal, shape (batch, channels, timesteps)
+
+        Returns:
+            y : output signal, shape (batch, channels, timesteps)
+
+        """
+        # expand coefficients to batch size and number of frames
+        b = self.coeffs.unsqueeze(0).unsqueeze(-1).expand(x.size(0), -1, ceil_division(x.size(-1), self.lfilter.hop_length))
+        # filter
+        return self.lfilter(x, b=b, a=None)
+
+    def emphasis(self, x: torch.Tensor) -> torch.Tensor:
+        """" Apply pre-emphasis to signal
+        Args:
+            x : input signal, shape (batch, channels, timesteps)
+
+        Returns:
+            y : output signal, shape (batch, channels, timesteps)
+
+        """
+        return self.forward(x)
+
+    def deemphasis(self, x: torch.Tensor) -> torch.Tensor:
+        """ Remove emphasis from signal
+        Args:
+            x : input signal, shape (batch, channels, timesteps)
+        Returns:
+            y : output signal, shape (batch, channels, timesteps)
+        """
+        # expand coefficients to batch size and number of frames
+        a = self.coeffs.unsqueeze(0).unsqueeze(-1).expand(x.size(0), -1, ceil_division(x.size(-1), self.lfilter.hop_length))
+        # filter
+        return self.lfilter(x, b=None, a=a)
