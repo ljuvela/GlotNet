f66040140f011eafa0d15c36511d8d9ed5d02167
Lauri Juvela <lauri.juvela@gmail.com>
2023-04-24 15:35:35 +0300

    move eigen to submodule dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     author='Lauri Juvela',
     license="",
     packages=['glotnet'],
-    package_dir={'glotnet': 'src/glotnet'},
     cmake_install_dir='glotnet',
     python_requires='>=3.7',
 )
