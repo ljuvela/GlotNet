bed440834f77c1a23edbc7e431c10b7dd2a32fb8
Lauri Juvela <lauri.juvela@gmail.com>
2023-04-24 16:07:01 +0300

    fix recursive packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages
 from skbuild import setup
 
+packages = find_packages('glotnet')
+packages = [f'glotnet.{p}' for p in packages]
+
 setup(
     name="glotnet",
     version="0.1.0",
     description="",
     author='Lauri Juvela',
     license="",
-    packages=['glotnet'],
+    packages=packages,
     cmake_install_dir='glotnet',
     python_requires='>=3.7',
 )
