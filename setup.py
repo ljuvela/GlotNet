bf82eb44bd3979eaf32b79d7588f0b3ee4fa4ea2
Lauri Juvela <lauri.juvela@aalto.fi>
2023-04-24 14:00:10 +0300

    pyproject.toml build system wip

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author='Lauri Juvela',
     license="",
     packages=['glotnet'],
+    package_dir={'glotnet': 'src/glotnet'},
     cmake_install_dir='glotnet',
     python_requires='>=3.7',
 )
